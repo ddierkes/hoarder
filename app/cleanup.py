41b9ce1404f9cf64bf4517ae15cbf1a6e7f825c4
Daron Dierkes <ddierkes@mohistory.org>
2018-12-11 17:28:34 -0600

    cleanup script improvements

--- app/cleanup.py
+++ app/cleanup.py
@@ -1,3 +1,6 @@
+"""The cleanup script is independent from the project and meant to be run as a cron.  It probably has deep flaws at scale"""
+
+
 import glob, os, time
 import sys
 from operator import itemgetter
@@ -9,11 +12,12 @@ def directory_cleanse(directory):
     IIIF file paths are identifier/region/size/rotation/quality.informat
     so this has to loop three times to remove rotation, then size, then region.
     """
+    print('\n\nEmpty Directories Removed:')
     for num in range(3):
         y = [x[0] for x in os.walk(directory)]
         for dir in y:
             if not os.listdir(dir):
-                print(f"removing {dir}")
+                print(f"\t {dir}")
                 os.rmdir(dir)
 
 
@@ -46,23 +50,31 @@ def chop_to_size(filelist, sizelimit, mode):
     sortedlist = sorted(filelist, key=itemgetter("date"))
     sortedlist = sortedlist[::-1]
     size = 0
-    lastfileindex = 0
-    print("\nKeep These:")
+    lastfileindex = -1
+    print("\n\nKeep These:")
     for index, f in enumerate(sortedlist):
-        print(f)
         size = size + f["size"]
-        if size > sizelimit:
+        if size > int(sizelimit):
             lastfileindex = index
+            size = size - f["size"]
             break
-    if lastfileindex > 0:
-        print("\nDiscard These:")
+        else:
+            print('\t' + str(f))
+    print(f'\n\tTotal Storage is: {size}')
+
+    bloat = 0
+    if lastfileindex > -1:
+        print("\n\nDiscard These:")
         for f in sortedlist[lastfileindex:]:
-            print(f)
+            print('\t'+str(f))
+            bloat = bloat + f["size"]
             if mode == "run":
                 os.remove(f["path"])
+        print(f'\n\tTotal Weight Loss is: {bloat}')
     else:
-        print("\nNothing to Discard.")
-    print("Total Memory Used Equals " + str(size))
+        print("\n\nNothing to Discard.")
+        return False
+    return True
 
 
 def main(mypath, sizelimit, mode="test", days=30):
@@ -81,14 +93,20 @@ def main(mypath, sizelimit, mode="test", days=30):
         filedict["date"] = os.stat(f).st_atime
         filelist.append(filedict)
     filelist = chop_to_date(filelist, days)
-    chop_to_size(filelist, sizelimit, mode)
+    deletion = chop_to_size(filelist, sizelimit, mode)
 
-    directory_cleanse(mypath)
+    if not deletion:
+        print("\nWith nothing to delete, there are no directories to remove.")
+    elif mode == "run":
+        directory_cleanse(mypath)
+    else:
+        print("\nThis is only a test.  To remove files use the 'run' mode")
 
 
 if __name__ == "__main__":
     # main("files", 2000000, "test", 1)
     try:
+        print(sys.argv)
         main(sys.argv[1], int(sys.argv[2]), sys.argv[3], int(sys.argv[4]))
     except:
         print(
